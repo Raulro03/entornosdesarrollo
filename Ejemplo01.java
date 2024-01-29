29512e77f1aae8e2f3de199a03ac9b4b8deeba62
Raulro03 <raulromeraortiz@gmail.com>
2024-01-29 19:27:31 +0100

    Añadir al EJEM01 nombre al comentario

--- Ejemplo01.java
+++ Ejemplo01.java
@@ -1,5 +1,7 @@
 public class Ejemplo01 {
     public static void main (String [] args) {
         System.out.println("Hola mundo");
+        String nombre = "Raul";
+        System.out.println("Mi nombre es: " + nombre);
     }
 }
